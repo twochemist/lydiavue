8bdc51f1683f4acc29901c4abf7f6a80854f6b69
Chemist Dev <33847652+twochemist@users.noreply.github.com>
2021-01-29 18:49:49 +0100

    Delete components directory

--- components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from '../..\\components\\Logo.vue'
-export { default as NavBar } from '../..\\components\\NavBar.vue'
-
-export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components_Logo'}" */).then(c => c.default || c)
-export const LazyNavBar = import('../..\\components\\NavBar.vue' /* webpackChunkName: "components_NavBar'}" */).then(c => c.default || c)
